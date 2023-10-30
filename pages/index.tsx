65947444f6deca274ade326e73c1fd62caf8b7e4
tomasbond02 <bondtomy@gmail.com>
2023-10-30 17:50:40 -0300

    agrego de inicio

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,37 @@ import type { NextPage } from 'next'
 import Router, { useRouter } from 'next/router'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import TextField from '@mui/material/TextField';
+import IconLabelButtons from '@/comoponents/buton';
 
 
 const Home: NextPage = () => {
   const router = useRouter()
   return (
     <div className={styles.general}>
-      <p>hola</p>
+      <div className={styles.containerInicio}>
+        <Image
+        src="/logoRivera.jpeg"
+        width={100}
+        height={100}
+        alt="Picture of the author"
+        />
+        <TextField
+        helperText="Please enter your username"
+        id="demo-helper-text-aligned"
+        label="USERNAME"
+        />
+        <TextField
+          helperText=" Please enter your password  "
+          id="demo-helper-text-aligned-no-helper"
+          label="PASSWORD"
+        />
+
+        <IconLabelButtons/>
+      </div>
     </div>
   )
 }
 
 export default Home
+
